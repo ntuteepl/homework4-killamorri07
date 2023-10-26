c0c0277026d030bd91303de8c762180b0197b63b
Lusia <34813844+killamorri07@users.noreply.github.com>
2023-10-26 18:38:53 +0800

    Update hw4-1.c

--- hw4-1.c
+++ hw4-1.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
+#include <math.h>
+
+int isPrime(int num) {
+    if (num <= 1) {
+        return 0; // 0 and 1 are not prime numbers
+    }
+    for (int i = 2; i <= sqrt(num); i++) {
+        if (num % i == 0) {
+            return 0; // num is divisible by i, so it's not prime
+        }
+    }
+    return 1; // num is prime
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if (n > 50) {
+        printf("Invalid input for n. It must be in the range (1, 50].\n");
+        return 1; // Exit with an error code
+    }
+
+    int myArray[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &myArray[i]);
+    }
+
+    // CHECKING PRIME NUMBER
+    for (int i = 0; i < n; i++) {
+        if (isPrime(myArray[i])) {
+            printf("YES");
+        } else {
+            printf("NO");
+        }
+    }
+
+    return 0; // Exit with success code
+}
 
