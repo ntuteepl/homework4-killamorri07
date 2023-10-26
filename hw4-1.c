449448e21f1485cbf97d13fe763b84a2bb6c2634
Lusia <34813844+killamorri07@users.noreply.github.com>
2023-10-26 18:43:04 +0800

    Update hw4-1.c

--- hw4-1.c
+++ hw4-1.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include <math.h>
 
 int isPrime(int num) {
     if (num <= 1) {
         return 0; // 0 and 1 are not prime numbers
     }
-    for (int i = 2; i <= sqrt(num); i++) {
+    for (int i = 2; i <= (num*num); i++) {
         if (num % i == 0) {
             return 0; // num is divisible by i, so it's not prime
         }
