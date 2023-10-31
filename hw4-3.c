d7dc8956ac8baddc78f990ebe935460ce85cc3ea
Lusia <34813844+killamorri07@users.noreply.github.com>
2023-10-31 11:42:59 +0800

    Update hw4-3.c

--- hw4-3.c
+++ hw4-3.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // Function to calculate the minimum number of vehicles needed
 int minVehiclesNeeded(int s[], int d[], int n) {
@@ -14,6 +15,7 @@ int minVehiclesNeeded(int s[], int d[], int n) {
         if (s[i] > currentEndTime) {
             // If the order can start after the current driver's end time, assign it to the current driver.
             currentEndTime = d[i];
+            vehiclesNeeded++;
         } else if (d[i] <= currentEndTime) {
             // If the order can be finished before or at the same time as the current driver's end time, do nothing.
             continue;
@@ -27,47 +29,113 @@ int minVehiclesNeeded(int s[], int d[], int n) {
     return vehiclesNeeded + 1; // Add 1 for the first driver
 }
 
-// Function to swap two integers
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
+// Function to sort values and store in the array
+void sortAndStore(int *d_array, int *r_array, int *n, int d, int r, int max_pairs) {
+    if (d > r) {
+        // Swap d and r if d is greater than r
+        int temp = d;
+        d = r;
+        r = temp;
+    }
 
-// Function to perform a bubble sort on the times array based on return times (d)
-void bubbleSort(int s[], int d[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (d[j] > d[j + 1]) {
-                // Swap return times (d) and corresponding start times (s)
-                swap(&d[j], &d[j + 1]);
-                swap(&s[j], &s[j + 1]);
-            }
-        }
+    if (*n < max_pairs) {
+        d_array[*n] = d;
+        r_array[*n] = r;
+        (*n)++;
     }
 }
 
 int main() {
-    int n; // Number of orders
-    scanf("%d", &n);
-    int s[n], d[n];
+    int s_array[100]; // Start times
+    int d_array[100]; // Departure times
+    int n = 0; // Initialize the order counter
+    int p; // Number of drivers
 
-    // Input: s1, d1, s2, d2, s3, d3 (departure and return times for three orders)
+    printf("Enter the number of drivers (p): ");
+    scanf("%d", &p);
+
+    while (1) {
+        int s, d;
+        int result = scanf("%d %d", &s, &d);
+
+        if (result == EOF) {
+            // End-of-file reached
+            break;
+        } else if (result != 2) {
+            // Invalid input, skip and continue
+            printf("Invalid input. Skipping...\n");
+            while (fgetc(stdin) != '\n');
+            continue;
+        }
+
+        s_array[n] = s;
+        d_array[n] = d;
+        n++;
+
+        if (n == 100) {
+            printf("Reached the maximum number of orders (100).\n");
+            break;
+        }
+    }
+
+    if (n == 0) {
+        printf("No orders to schedule.\n");
+        return 0;
+    }
+
+    // Sort the orders based on departure times
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (d_array[j] > d_array[j + 1]) {
+                int temp = d_array[j];
+                d_array[j] = d_array[j + 1];
+                d_array[j + 1] = temp;
+                temp = s_array[j];
+                s_array[j] = s_array[j + 1];
+                s_array[j + 1] = temp;
+            }
+        }
+    }
+
+    // Initialize an array of arrays to store driver schedules
+    int **driverSchedules = (int **)malloc(p * sizeof(int *));
+    for (int i = 0; i < p; i++) {
+        driverSchedules[i] = (int *)malloc(100 * sizeof(int));
+    }
+
+    // Initialize array to track driver schedule sizes
+    int *scheduleSizes = (int *)malloc(p * sizeof(int));
+    for (int i = 0; i < p; i++) {
+        scheduleSizes[i] = 0;
+    }
+
+    // Assign orders to drivers
     for (int i = 0; i < n; i++) {
-        scanf("%d %d", &s[i], &d[i]);
+        int assignedDriver = -1;
+        for (int j = 0; j < p; j++) {
+            if (s_array[i] >= d_array[i] || (scheduleSizes[j] > 0 && s_array[i] < driverSchedules[j][scheduleSizes[j] - 1])) {
+                // If the order has no duration (s >= d) or it overlaps with a previous order of the driver, skip it.
+                continue;
+            }
+
+            assignedDriver = j;
+            driverSchedules[j][scheduleSizes[j]++] = d_array[i];
+            break;
+        }
+
+        if (assignedDriver == -1) {
+            printf("Cannot accommodate all orders. Additional drivers needed.\n");
+            for (int i = 0; i < p; i++) {
+                free(driverSchedules[i]);
+            }
+            free(driverSchedules);
+            free(scheduleSizes);
+            return 0;
+        }
     }
 
-    // Sort the orders based on return times (d)
-    bubbleSort(s, d, n);
-
-    // Calculate and print the minimum number of vehicles needed
-    int vehiclesNeeded = minVehiclesNeeded(s, d, n);
-
-    if (vehiclesNeeded != -1) {
-        printf("%d\n", vehiclesNeeded);
-    } else {
-        printf("Cannot accommodate all orders.\n");
-    }
-
-    return 0;
-}
+    // Print driver schedules
+    for (int i = 0; i < p; i++) {
+        printf("Driver %d Schedule: ", i + 1);
+        for (int j = 0; j < scheduleSizes[i]; j++) {
+            printf("(%d, %d) ", s_array[j],
